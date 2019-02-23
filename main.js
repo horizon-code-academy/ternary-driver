55e8590c5918b918f1815f753072638081557ead
Malek Boubakri <malekbouba@gmail.com>
2019-02-23 20:24:55 +0100

    finalisation

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ document.querySelector('#submit').addEventListener("click", () => {
         name: document.querySelector('#name').value || "Anonymous",
         age,
         lic: age < 18 ? 0 : licDate,
-        drive:  age < 18 || age > 65 ? "No" : licDate >= 2 ? "Expert" : "Yes",
+        drive: age < 18 || age > 65 ? "No" : licDate >= 2 ? "Expert" : "Yes",
     };
     document.querySelector('.result').innerHTML = `
     <strong>Name:</strong>
     <br/> ${person.name}<br/>
     <strong>Age:</strong>
-    <br/> ${person.age} ans<br/>
+    <br/> ${person.age} year${person.age < 1 || 's'}<br/>
     <strong>License date:</strong>
-    <br/> ${person.lic} ans<br/>
+    <br/> ${person.lic} year${person.lic < 1 || 's'}<br/>
     <strong>Is driver:</strong>
     <br/> ${
         person.drive === "No"
