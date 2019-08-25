db2b2b4a3675d7b05bf625d02b1f1c81d0a945bb
Malek BOUBAKRI <malekbouba@gmail.com>
2019-08-25 23:49:05 +0100

    v2 changes

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
+document.querySelector("#age").addEventListener("change", () => {
+  const age = document.querySelector("#age").value;
+  if (parseInt(age) < 18) {
+    document.querySelector("label[for='hasLicense']").style.display = "none";
+    document.querySelector("#hasLicense").style.display = "none";
+    document.querySelector("label[for='license']").style.display = "none";
+    document.querySelector("#license").style.display = "none";
+  } else {
+    document.querySelector("label[for='hasLicense']").style.display = "block";
+    document.querySelector("#hasLicense").style.display = "block";
+    document.querySelector("label[for='license']").style.display = "block";
+    document.querySelector("#license").style.display = "block";
+  }
+})
+
 document.querySelector("#submit").addEventListener("click", () => {
   const age = document.querySelector("#age").value;
-  const licDate = document.querySelector("#lic").value;
+  const license = document.querySelector("#license").value;
+  const hasLicense = document.querySelector("#hasLicense").value;
+  const gender = document.querySelector("#gender").value;
 
   const person = {
     name: document.querySelector("#name").value || "Anonymous",
+    lastName: document.querySelector("#lastname").value || "Anonymous",
     age,
-    lic: age < 18 ? 0 : licDate,
-    drive: age < 18 || age > 65 ? "No" : licDate >= 2 ? "Expert" : "Yes"
+    gender,
+    hasLicense,
+    license: age < 18 ? 0 : license,
+    drive: age < 18 || age > 65 ? "No" : license >= 2 ? "Expert" : "Yes"
   };
 
-  const textPlot = ({name, age, lic, drive}) => `
+  const textPlot = p => `
     <strong>Name:</strong>
-    <br/> ${name}<br/>
+    <br/> ${p.gender === "male" ? "Mr." : "Mme."} ${p.name} ${p.lastName}<br/>
     <strong>Age:</strong>
-    <br/> ${age} year${age > 1 ? "s" : ""}<br/>
+    <br/> ${p.age} year${p.age > 1 ? "s" : ""}<br/>
     <strong>License date:</strong>
-    <br/> ${lic} year${lic > 1 ? "s" : ""}<br/>
+    <br/> ${p.license} year${p.license > 1 ? "s" : ""}<br/>
     <strong>Is driver:</strong>
     <br/> ${
-        drive === "No"
-        ? `<span style="color: red">No</span>`
-        : drive === "Yes"
+    p.drive === "No"
+      ? `<span style="color: red">No</span>`
+      : p.drive === "Yes"
         ? `<span style="color: green">Yes</span>`
         : `<span style="color: blue">Expert</span>`
     }<br/>
