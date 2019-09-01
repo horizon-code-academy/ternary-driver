d75ff888cac228f5a3945c81b3fba4baf27d8841
hamdi ben abdelhafidh <hamdi.babdelhafidh@gmail.com>
2019-09-01 16:58:03 +0100

    final

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+document.querySelector("#submit").addEventListener("click", () => {
+  const name = document.querySelector("#name").value;
+  const lastName = document.querySelector("#lastname").value;
+  const age = document.querySelector("#age").value;
+  const license = document.querySelector("#license").value;
+  const hasLicense = document.querySelector("#hasLicense").value;
+  const gender = document.querySelector("#gender").value;
+  const person = {
+    name,
+    lastName,
+    age: age || 0,
+    gender,
+    hasLicense,
+    license: age < 18 ? 0 : license,
+  };
+  console.log(person)
+  document.querySelector(".result").innerHTML =
+    `<strong>Name:</strong>
+      <br/> ${person.gender === "male" ? "Mr." : "Mme."} ${person.name} ${person.lastName}<br/>
+      <strong>Age:</strong>
+      <br/> ${person.age} year${person.age > 1 ? "s" : ""}<br/>
+      <strong>License date:</strong>
+      <br/> ${person.license} year${person.license > 1 ? "s" : ""}<br/>
+      <strong>Is driver:</strong>
+      <br/> ${
+    age < 18 || age > 65
+      ? `<span style="color: red">Not a driver</span>`
+      : license < 2
+        ? `<span style="color: green">Beginner</span>`
+        : `<span style="color: blue">Expert</span>`
+    }<br/>
+    `
+});
+
+/**
+ * Form control and hide/show input conditionally.
+ */
 document.querySelector("#age").addEventListener("change", () => {
   const age = document.querySelector("#age").value;
   if (parseInt(age) < 18) {
@@ -11,39 +48,15 @@ document.querySelector("#age").addEventListener("change", () => {
     document.querySelector("label[for='license']").style.display = "block";
     document.querySelector("#license").style.display = "block";
   }
-})
-
-document.querySelector("#submit").addEventListener("click", () => {
-  const age = document.querySelector("#age").value;
-  const license = document.querySelector("#license").value;
+});
+document.querySelector("#hasLicense").addEventListener("change", () => {
   const hasLicense = document.querySelector("#hasLicense").value;
-  const gender = document.querySelector("#gender").value;
-
-  const person = {
-    name: document.querySelector("#name").value || "Anonymous",
-    lastName: document.querySelector("#lastname").value || "Anonymous",
-    age,
-    gender,
-    hasLicense,
-    license: age < 18 ? 0 : license,
-    drive: age < 18 || age > 65 ? "No" : license >= 2 ? "Expert" : "Yes"
-  };
-
-  const textPlot = p => `
-    <strong>Name:</strong>
-    <br/> ${p.gender === "male" ? "Mr." : "Mme."} ${p.name} ${p.lastName}<br/>
-    <strong>Age:</strong>
-    <br/> ${p.age} year${p.age > 1 ? "s" : ""}<br/>
-    <strong>License date:</strong>
-    <br/> ${p.license} year${p.license > 1 ? "s" : ""}<br/>
-    <strong>Is driver:</strong>
-    <br/> ${
-    p.drive === "No"
-      ? `<span style="color: red">No</span>`
-      : p.drive === "Yes"
-        ? `<span style="color: green">Yes</span>`
-        : `<span style="color: blue">Expert</span>`
-    }<br/>
-    `;
-  document.querySelector(".result").innerHTML = textPlot(person);
+  console.log(hasLicense)
+  if (hasLicense === "no") {
+    document.querySelector("label[for='license']").style.display = "none";
+    document.querySelector("#license").style.display = "none";
+  } else {
+    document.querySelector("label[for='license']").style.display = "block";
+    document.querySelector("#license").style.display = "block";
+  }
 });
